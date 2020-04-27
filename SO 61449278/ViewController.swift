b054755e07c2cf2338225f5feafc3c9233c27595
Aaron Cyrman <acyrman@icyrman.com>
2020-04-26 20:18:32 -0600

    Initial Commit

---
+++ SO 61449278/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SO 61449278
+//
+//  Created by acyrman on 4/26/20.
+//  Copyright © 2020 iCyrman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
---
+++ SO 61449278Tests/SO_61449278Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SO_61449278Tests.swift
+//  SO 61449278Tests
+//
+//  Created by acyrman on 4/26/20.
+//  Copyright © 2020 iCyrman. All rights reserved.
+//
+
+import XCTest
+@testable import SO_61449278
+
+class SO_61449278Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
